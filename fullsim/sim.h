22391ebc877f00e8cc707b6d435f1db3320d2ecc
curt mayer <curt@zen-room.org>
2019-08-02 17:16:18 -0700

    more io goodness

--- fullsim/sim.h
+++ fullsim/sim.h
@@ -1,13 +1,14 @@
 /*
  * global definitions for simulator
  */
+typedef unsigned char byte;
+
 typedef unsigned long paddr;	// 24 bit physical address
 typedef unsigned short vaddr;	// 16 bit virtual address
-typedef unsigned char portaddr;
-typedef unsigned char portdata;
+typedef byte portaddr;
 
-typedef portdata (*inhandler)(portaddr port);
-typedef void (*outhandler)(portaddr port, portdata val);
+typedef byte (*inhandler)(portaddr port);
+typedef void (*outhandler)(portaddr port, byte val);
 
 extern inhandler input_handler[256];
 extern outhandler output_handler[256];
@@ -17,13 +18,13 @@ extern void register_input(portaddr portnum, inhandler func);
 extern void register_output( portaddr portnum, outhandler func);
 
 extern void ioinit();
-extern void output(portaddr p, portdata v);
-extern portdata input(portaddr p);
+extern void output(portaddr p, byte v);
+extern byte input(portaddr p);
 
 extern void ioinit();
-extern void output(portaddr p, portdata v);
+extern void output(portaddr p, byte v);
 
-extern portdata input(portaddr p);
+extern byte input(portaddr p);
 
 int imd_read(void *ip, int drive, int trk, int side, int sec, char *buf);
 void *load_imd(char *fname);
