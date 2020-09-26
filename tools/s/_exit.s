91c7a69419d7e6311959715c2f37abdbd64f1539
curt mayer <curt@zen-room.org>
2020-09-26 00:20:01 -0700

    almost done

--- tools/s/_exit.s
+++
@@ -1,7 +0,0 @@
-# z80 disassembly
-__exit:
-	POP BC                         ;       c1                 .     
-	POP HL                         ;       e1                 .     
-	PUSH HL                        ;       e5                 .     
-	PUSH BC                        ;       c5                 .     
-	SYS EXIT                       ;       cf 01              ..    
--- tools/s/getpid.s
+++
@@ -1,6 +0,0 @@
-# z80 disassembly
-_getpid:
-	SYS GETPID                     ;       cf 14              ..    
-	LD C,L                         ;       4d                 M     
-	LD B,H                         ;       44                 D     
-	RET                            ;       c9                 .     
