b7d86c052eb1aa3a6d96dbeb60e62b1b44bcdfb2
Curt Mayer <curt@zen-room.org>
2020-11-27 23:28:42 -0800

    bdos calls found

--- src/tools/hitech/hitpatch.c
+++ src/tools/hitech/hitpatch.c
@@ -19,56 +19,169 @@ int traceflags;
 
 typedef unsigned short addr_t;
 
+/*
+ * addresses grunged out of our object file
+ */
+addr_t a_bss;
+addr_t a_end;
+addr_t a_exit;
+addr_t a_main;
+addr_t a_startup;
+addr_t a_getarg;
 addr_t a_getuid;
 addr_t a_setuid;
+
 addr_t a_bdoshl;
+addr_t a_bdos;
 addr_t a_bdosa;
 
-struct pattern {
+addr_t a_write;
+addr_t a_csv;
+addr_t a_ncsv;
+
+/*
+ * system call library functions
+ */
+addr_t a_writeaddr;
+
+struct var {
+	addr_t *varp;
 	char *name;
-	addr_t *var;
-	int baseoffset;
-	int patoff;
-	int patlen;
-	short p[20];	/* -1 matches any byte */
 };
 
-struct pattern pats[] = {
-	{ "getuid", &a_getuid, 9, -6, 6, 
-		{ 0x0e, 0x20, 0x1e, 0xff, 0xdd, 0xe5 } },
-	{ "setuid", &a_setuid, 9, 3, 6, 
-		{ 0xdd, 0x5e, 0x08, 0x1e, 0xff, 0xdd, 0xe5 } },
-	{ "bdoshl", &a_bdoshl, 9, 3, 6, 
-		{ 0xdd, 0x5e, 0x08, 0x1e, 0xff, 0xdd, 0xe5 } },
-	{ "bdosa", &a_bdosa, 9, 3, 6, 
-		{ 0xdd, 0x5e, 0x08, 0x1e, 0xff, 0xdd, 0xe5 } },
+struct var getarg_v = {
+	&a_getarg, "getarg"
+};
+
+struct get {
+	struct var v;
+	addr_t where;
+} crtvars[] = {
+	{{ &a_bss, "bss" }, 0x105 },
+	{{ &a_end, "end" }, 0x109 },
+	{{ &a_startup, "startup" }, 0x12a },
+	{{ &a_main, "_main" }, 0x134 },
+	{{ &a_exit, "_exit" }, 0x138 },
+	{0}
+};
+
+/*
+ * patterns to matchg subroutines that contain a call to a known address
+ */
+struct pattern {
+	char *name;		/* function */
+	addr_t *var;	/* pointer where to store offset */
+	int patoff;		/* where to start pattern match */
+	short sig[40];
+#define	ANY	-1
+#define	END	-2
+};
+
+/*
+ * zcrt
+ */
+short zcrtpat[] = {
+	0x2a, 0x06, 0x00, 
+	0xf9, 
+	0x11, ANY, ANY, 
+	0xb7, 
+	0x21, ANY, ANY, 
+	0xed, 0x52, 
+	0x4d, 
+	0x44, 
+	0x0b, 0x6b, 0x62, 0x13, 
+	0x36, 0x00, 
+	0xed, 0xb0, 
+	0x21, ANY, ANY, 
+	0xe5, 
+	0x21, 0x80, 0x00, 
+	0x4e, 
+	0x23, END
+};
+
+/*
+ * there are only a few places that call 5.  find them all
+ */
+struct pattern entry_pats[] = {
+	{ "bdos", &a_bdos, -16, { 
+		0xcd, ANY, ANY, 
+		0xdd, 0x5e, 0x08, 
+		0xdd, 0x56, 0x09, 
+		0xdd, 0x4e, 0x06,
+		0xdd, 0xe5,
+		0xfd, 0xe5,
+		0xcd, 0x05, 0x00, 
+		0xfd, 0xe1,
+		0xdd, 0xe1,
+		0x6f, 
+		0x17, 
+		0x9f, 
+		0x67, 
+		0xc3, ANY, ANY,  END } },
+	{ "bdosa", &a_bdosa, -14, { 
+		0xcd, ANY, ANY, 
+		0xdd, 0x5e, 0x08, 
+		0xdd, 0x56, 0x09, 
+		0xdd, 0x4e, 0x06,
+		0xdd, 0xe5,
+		0xcd, 0x05, 0x00, 
+		0xdd, 0xe1,
+		0x6f, 
+		0x17, 
+		0x9f, 
+		0x67, 
+		0xc3, ANY, ANY,  END } },
+	{ "bdoshl", &a_bdoshl, -14, {
+		0xcd, ANY, ANY,
+		0xdd, 0x5e, 0x08, 
+		0xdd, 0x56, 0x09, 
+		0xdd, 0x4e, 0x06,
+		0xdd, 0xe5,
+		0xcd, 0x05, 0x00, 
+		0xdd, 0xe1,
+		0xc3, ANY, ANY, END } },
+	{ "getuid", &a_getuid, -9, { 
+		0xcd, ANY, ANY, 
+		0x0e, 0x20,
+		0x1e, 0xff,
+		0xdd, 0xe5,
+		0xcd, 0x05, 0x00, END } },
+	{ "setuid", &a_setuid, -10, {
+		0xcd, ANY, ANY, 
+		0xdd, 0x5e, 0x06,
+		0x0e, 0x20,
+		0xdd, 0xe5,
+		0xcd, 0x05, 0x00, END } },
 	{ 0 }
 };
 
-#ifdef nodef
-			printf("bdos called at %x\n", i);
-			if (mem[i-6] == 0xe && mem[i-5] == 0x20) {
-				s = "getuser";
-			}
-			if (mem[i-4] == 0xe && mem[i-3] == 0x20) {
-				s = "setuser";
-			}
-			if (mem[i+3] == 0xdd && mem[i+4] == 0xe1 && mem[i+5] == 0xc3) {
-				s = "bdoshl";
-			}
-			if (mem[i+3] == 0xfd && mem[i+4] == 0xe1 && mem[i+5] == 0xdd) {
-				s = "bdosa";
-			}
-#endif
+struct pattern bdos_pats[] = {
+	{ "write", &a_write, -0x6c, {
+		0xcd, ANY, ANY, 
+		0x79, 0xff, 
+		0x06, 0x08, 0xdd, 
+		  0x7e, 0x06, 0xcd, ANY, ANY, 0x38, 0x06, 0x21,
+		  0xff, 0xff, 0x11, 0x2a, 0x00, 0xdd, 0x6e, 0x06, END } },
+	{ 0 }
+};
+
+getptr(struct var *v, addr_t where)
+{
+	addr_t a;
+	a = mem[where] + ((mem[where+1]) << 8);
+	*v->varp = a;
+
+	if (verbose) printf("%s: %04x\n", v->name, a);
+}
 
 /*
- * match pattern p against memory b for len
+ * match pattern p against memory b
  */
 int
-patmatch(short *p, unsigned char *b, int len)
+patmatch(short *p, unsigned char *b)
 {
-	while (len--) {
-		if (*p == -1) {
+	while (*p != END) {
+		if (*p == ANY) {
 			p++; b++; continue;
 		}
 		if (*p++ != *b++) return 0;
@@ -79,23 +192,23 @@ patmatch(short *p, unsigned char *b, int len)
 /*
  * iterate through all the patterns checking for hits
  */
-int
-lookup_pat(int addr)
+struct pat *
+lookup_pat(int addr, struct pattern *p)
 {
-	int i;
-	for (i = 0; pats[i].name; i++) {
-		if (patmatch(pats[i].p, &mem[addr + pats[i].patoff], pats[i].patlen))
-			return i;
+	while (p->name) {
+		if (patmatch(p->sig, &mem[addr + p->patoff]))
+			return p;
+		p++;
 	}
-	return -1;
+	return 0;
 }
 
 /*
  * gripe and die
  */
-lose(char *s)
+lose(char *f, char *s)
 {
-	fprintf(stderr, "lose: %d\n", s);
+	fprintf(stderr, "lose: %s %s\n", f, s);
 	exit(1);
 }
 
@@ -132,9 +245,8 @@ main(int argc, char **argv)
 		} else break;
 		argv++;
 	}	
-	printf("verbose = %d\n", verbose);
-	if (argc) {
-		process(*argv);
+	while (argc--) {
+		process(*argv++);
 	}
 	exit (0);
 }
@@ -176,35 +288,78 @@ process(char *fn)
 	int fd;
 	addr_t i;
 	int j;
+	struct pattern *p;
 
 	if (verbose) printf("do file %s\n", fn);
 
 	if (stat(fn, &sbuf) != 0) {
-		lose("cannot stat");
+		lose(fn, "cannot stat");
 	}
 	size = sbuf.st_size;
 	
 	mem = calloc(65536, 1);
-	if (!mem) lose("cannot allocate memory");
+	if (!mem) lose("cannot allocate memory", "");
 	if (verbose) printf("size = %d\n", size);
 	fd = open(fn, 0);
+	if (fd < 0) {
+		lose(fn, "can't open");
+	}
 	i = read(fd, &mem[0x100], 65536);
 	if (i != size) {
-		lose("file length read");
+		lose(fn, "file length read");
 	}
+	close(fd);
+
+	/*
+	 * first, make sure it's a hitech C binary, with
+	 * the zcrt prolog.
+	 */
+	if (!patmatch(zcrtpat, &mem[0x100])) {
+		fprintf(stderr, "%s: not a hitech binary\n", fn);
+		return;
+	}
+	if (verbose) printf("%s: hitech detected\n", fn);	
+
+	/* read addresses from crt */
+	for (i = 0; crtvars[i].v.name; i++) {
+		getptr(&crtvars[i].v, crtvars[i].where);
+	}
+	/* get address of getarg */
+	getptr(&getarg_v, a_startup + 1);
 
 	/*
 	 * let's find all the bdos calls and classify them
 	 */
 	for (i = 0x100; (i = next_callto(i, 0x0005)) != 0xffff; i += 3) {
-		j = lookup_pat(i);
-		if (j == -1) {
+		p = lookup_pat(i, entry_pats);
+		if (!p) {
 			printf("unknown function at %x\n", i);
 			dumpmem(memget, i-0x20, 0x40);
 		} else {
-			printf("function %s at %x\n", 
-				pats[j].name, i - pats[j].baseoffset);
-			dumpmem(memget, i-0x20, 0x40);
+			printf("function %s at %x\n", p->name, i + p->patoff);
+			*p->var = i + p->patoff;
+		}
+	}
+
+	/*
+	 * now, let's find all the calls to the ones we just found
+	 */
+	if (a_bdos) {
+		for (i = 0x100; (i = next_callto(i, a_bdos)) != 0xffff; i += 3) {
+			printf("bdos call at %x\n", i);
+		}
+	}
+
+	if (a_bdosa) {
+		for (i = 0x100; (i = next_callto(i, a_bdosa)) != 0xffff; i += 3) {
+			printf("bdosa call at %x\n", i);
+		}
+	}
+
+	if (a_bdoshl) {
+		for (i = 0x100; (i = next_callto(i, a_bdoshl)) != 0xffff; i += 3) {
+			printf("bdoshl call at %x\n", i);
 		}
 	}
 }
+
