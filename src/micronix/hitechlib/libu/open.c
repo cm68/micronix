db36cc16c5e0c519446217ea2bd334aaa690bd64
curt mayer <curt@zen-room.org>
2020-12-25 18:54:36 -0800

    start of micronix system lib

---
+++ src/micronix/hitechlib/libu/open.c
@@ -0,0 +1,21 @@
+#asm
+	psect text
+	global _errno
+	global _open
+_open:
+	pop bc				; bc = retaddr
+	pop hl				; hl = mode
+	ld (sys_op+4),hl	
+	pop hl				; hl = name
+	ld (sys_op+2),hl
+	push bc				; restore stack size
+	push bc
+	push bc
+sys_op:
+	defb 0cfH, 5, 0, 0, 0, 0
+	ret nc
+	ld (_errno),hl
+	ld hl,-1
+	ret
+#endasm
+
