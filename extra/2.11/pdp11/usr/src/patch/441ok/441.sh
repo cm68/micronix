b0e2bf1a7acfc1755f0d14cda0873fbbfcb64100
curt mayer <curt@zen-room.org>
2021-09-05 21:11:37 -0700

    added 2.11, more cleanup

---
+++ extra/2.11/pdp11/usr/src/patch/441ok/441.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -f /usr/man/cat2/creat.0
+rm -f /usr/src/man/man2/creat.2
---
+++ extra/2.11/pdp11/usr/src/sys/CURLY/vers.c
@@ -0,0 +1,1 @@
+char version[] = "2.11 BSD UNIX #153: Thu Jun 8 23:53:05 PDT 1995\n    root@curly.2bsd.com:/usr/src/sys/CURLY\n";
