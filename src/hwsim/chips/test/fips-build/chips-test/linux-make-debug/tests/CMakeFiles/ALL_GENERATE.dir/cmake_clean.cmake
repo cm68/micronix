f2cb9498f28e8e3574191db323a665de0b755a48
curt mayer <curt@zen-room.org>
2023-06-23 00:25:13 -0700

    more reorg

--- src/hwsim/chips/test/fips-build/chips-test/linux-make-debug/tests/CMakeFiles/ALL_GENERATE.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ALL_GENERATE"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ALL_GENERATE.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
